b7d9737c369847ea1ebb024c85417d101c8d97e9
thephunkmonk <andrewchoi1218@gmail.com>
2024-12-04 16:13:09 +0900

    changed crud to just take json now

--- fastapi/src/mongo/crud.py
+++ fastapi/src/mongo/crud.py
@@ -1,16 +1,17 @@
 import requests
 from src.mongo.models import Movie
 from src.mongo.database import engine
+from typing import Dict, List
 
 # 영화 저장 함수
-async def save_movie_to_db(movie_id: int):
+async def save_movie_to_db(movie_data: Dict):
     # 기존 데이터 확인
     existing_movie = await engine.find_one(Movie, Movie.movie_id == movie_id)
     if existing_movie:
         return {"message": f"Movie '{existing_movie.title}' already exists in the database."}
 
     #필요항목 확인단계
-    required_fields = ["movie_id", "title", "genres", "cast", "director", "release_year", "country", "popularity"]
+    required_fields = ["movie_id", "title", "original_title", "overview", "poster_path", "original_country", "genres", "release_date", "cast", "director"]
     missing_fields = [field for field in required_fields if field not in movie_data]
     if missing_fields:
         raise HTTPException(
@@ -20,16 +21,16 @@ async def save_movie_to_db(movie_id: int):
 
     # MongoDB 저장
     movie = Movie(
-        movie_id=detail_data["id"],
-        title=detail_data.get("title"),
-        original_title=detail_data.get("original_title"),
-        overview=detail_data.get("overview"),
-        poster_path=detail_data.get("poster_path"),
-        original_country=detail_data.get("origin_country"),
-        genres=genres,
-        release_date=detail_data.get("release_date"),
-        cast=cast,
-        director=director,
+        movie_id=movie_data["id"],
+        title=movie_data["title"],
+        original_title=movie_data["original_title"],
+        overview=movie_data["overview"],
+        poster_path=movie_data["poster_path"],
+        original_country=movie_data["origin_country"],
+        genres=movie_data['genres'],
+        release_date=movie_data["release_date"],
+        cast=movie_data["cast"],
+        director=movie_data["director"],
     )
     await engine.save(movie)
     return {"message": f"Movie '{movie.title}' has been saved to the database."}
