fed99444e94c528309820a7e109993ac3d0193bd
thephunkmonk <andrewchoi1218@gmail.com>
2024-12-04 15:41:48 +0900

    edits to fastapi crud

--- fastapi/src/mongo/crud.py
+++ fastapi/src/mongo/crud.py
@@ -9,34 +9,14 @@ async def save_movie_to_db(movie_id: int):
     if existing_movie:
         return {"message": f"Movie '{existing_movie.title}' already exists in the database."}
 
-    # DETAIL API 호출
-    detail_url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=ko-KR"
-    headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmZlYzEwOGU3M2Y3YmVmNTkzYzM3N2RjMzdjYjcyZCIsIm5iZiI6MTczMjg2MjU3NC43Miwic3ViIjoiNjc0OTYyNmU0OTE5MDljMWI3OWRlY2VkIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.aumSeBsjfSdLck30QaMJjzeLi7ZZ4CMBOZS20p_AVdw"
-    }
-    detail_response = requests.get(detail_url, headers=headers)
-    detail_data = detail_response.json()
-
-    # CREDIT API 호출
-    credit_url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?language=ko-KR"
-    credit_response = requests.get(credit_url, headers=headers)
-    credit_data = credit_response.json()
-
-    # 데이터 정리
-    genres = [genre["name"] for genre in detail_data.get("genres", [])]
-    cast = sorted(
-        [{"cast_id": c["cast_id"], "name": c["name"]} for c in credit_data.get("cast", [])],
-        key=lambda x: x["cast_id"]
-    )[:8]
-    director = next(
-        (
-            {"id": crew["id"], "name": crew["name"]}
-            for crew in credit_data.get("crew", [])
-            if crew.get("job") == "Director"
-        ),
-        None
-    )
+    #필요항목 확인단계
+    required_fields = ["movie_id", "title", "genres", "cast", "director", "release_year", "country", "popularity"]
+    missing_fields = [field for field in required_fields if field not in movie_data]
+    if missing_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Missing required fields: {', '.join(missing_fields)}"
+        )
 
     # MongoDB 저장
     movie = Movie(
