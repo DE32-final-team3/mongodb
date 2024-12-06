24458229aac19e94051045572d63abeedeac7d39
Seokxkyu <kyuseok000@gmail.com>
2024-12-06 19:23:28 +0900

    update server

--- fastapi/src/mongo/database.py
+++ fastapi/src/mongo/database.py
@@ -2,8 +2,9 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from odmantic import AIOEngine
 import os
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:team3@localhost:27017/")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "movies")
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:team3@localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:cine@3.37.94.149:27017/")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "cinetalk")
 
 motor_client = AsyncIOMotorClient(MONGO_URI)
 
