f24874a7eb8d5724081efca2af5fdbfc692f1376
Seokxkyu <kyuseok000@gmail.com>
2024-12-16 10:02:43 +0900

    updates

--- fastapi/src/final_backend/database.py
+++ fastapi/src/final_backend/database.py
@@ -3,10 +3,15 @@ from odmantic import AIOEngine
 import os
 
 # MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:team3@localhost:27017/")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:cine@3.37.94.149:27017/")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "cinetalk")
+# MONGO_URI = os.getenv("MONGO_URI", "mongodb://root:cine@3.37.94.149:27017/")
+# DATABASE_NAME = os.getenv("DATABASE_NAME", "cinetalk")
 
-motor_client = AsyncIOMotorClient(MONGO_URI)
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_IP = os.getenv("DB_IP")
+DB_PORT = os.getenv("DB_PORT")
+DATABASE_URL = f"mongodb://root:{DB_PASSWORD}@{DB_IP}:{DB_PORT}"
 
-engine = AIOEngine(motor_client=motor_client, database=DATABASE_NAME)
+# client = AsyncIOMotorClient(MONGO_URI)
+client = AsyncIOMotorClient(DATABASE_URL)
 
+engine = AIOEngine(client=client, database="cinetalk")
